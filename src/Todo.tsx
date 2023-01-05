a387bfb0992bafff4ebc4f9bd9dc9cdefa7d770c
tommy_cheung <tcheung@hk.palo-it.com>
2023-01-05 12:14:53 +0800

    add state in todo

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 export default function Todo() {
+	const [todo, setTodo] = useState("");
 	return (
 		<>
 			<h1>Todo List Page</h1>
 			<p>tell me what you want to add: </p>
 			<form action="submit">
-				<input type="text" />
-				<button type="submit">add!</button>
+				<input type="text" onChange={(e) => setTodo(e.target.value)} />
+				<button
+					type="submit"
+					onClick={(e) => {
+						e.preventDefault();
+						console.log(todo);
+					}}
+				>
+					add!
+				</button>
 			</form>
 		</>
 	);
