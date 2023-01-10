9aba4d0997140e0f869208bf29f780df09db51fb
tommycheung1989 <tommycheung1989@gmail.com>
2023-01-10 20:27:52 +0800

    change test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const todo = screen.getByTestId('todo');
+  const todo = screen.getByText('Todo List Page');
   expect(todo).toBeInTheDocument();
 });
